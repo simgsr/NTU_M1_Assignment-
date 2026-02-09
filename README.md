8adf6d2dbac2cb0a88821b5147c91bda54e7e9dd
khoonseng <khoonseng87@gmail.com>
2026-02-10 00:18:33 +0800

    updated EDA steps and updated README.md

--- README.md
+++ README.md
@@ -18,3 +18,37 @@ conda env create -f environment.yml
 
 # Activate the environment
 conda activate ntu-m1-assignment
+```
+
+## Steps performed to clean raw data
+### (1) check dataframe if "categories" column contain blank entries (either empty or string with spaces)
+- create a boolean masking "blank_categories_mask" to filter out blank entries
+- 3,988 rows are blank and are removed from dataset
+
+### (2) check columns with data that are not meaningful
+- drop occupationId column as it contains no data
+- drop status_id column as it contains non-informative data
+
+### (3) check data accuracy for min/max/avg salary
+- salary_minimum cannot be higher than salary_maximum
+- average_salary must be between salary_minimum and salary_maximum
+- no data issues observed
+
+### (4) check for any duplicate job post id with "metadata_jobPostId" column
+- no duplicates observed
+
+### (5) check if text columns (postedCompany_name, title) contain inconsistent cases and special characters
+- 447 out of 53,151 company names are in upper case. to standardise all company names to be capitalized
+- replace ampersand to "and"
+- remove special characters with regex
+- remove leading and trailing whitespace
+- replace multiple spaces with a single space
+
+### (6) identify the employment types of jobs to focus on
+- based on employment types, to focus only on 95% of jobs: Permanent (44%), Full Time (38%) and Contract (13%)
+- to remove jobs with titles that are meant for excluded employment types e.g. parttime/part time, intern, temp
+- to remove jobs with titles that contain hourly rates
+- to remove outliers where average salary > 400,000
+- to remove low paying jobs where average salary < 4,000
+
+
--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import common_utilities as utils
+import re
 
 # Suppress scientific notation globally
 pd.set_option('display.float_format', '{:.2f}'.format)
@@ -16,6 +17,9 @@ df = pd.read_csv(csv_file_path)
 #print(df.info(()))
 #print(df.columns.tolist())
 
+'''
+STEP 1 - check dataframe if "categories" column contain blank entries (either empty or string with spaces)
+'''
 # check dataframe if columns contain blank entries
 utils.check_dataframe_for_blanks(df)
 
@@ -23,7 +27,8 @@ utils.check_dataframe_for_blanks(df)
 utils.check_column_contains_blanks(df['categories'])
 
 # filter dataframe for blank entries in categories column and export to csv
-blank_categories_mask = df['categories'].isna() | (df['categories'].astype(str).str.strip() == '')
+blank_categories_mask = (df['categories'].isna() 
+                         | (df['categories'].astype(str).str.strip() == ''))
 blank_categories_df = df[blank_categories_mask]
 output_file = "data/blank_categories.csv"
 #utils.export_dataframe_to_csv(blank_categories_df,output_file)
@@ -41,6 +46,9 @@ utils.check_dataframe_for_blanks(filtered_df)
 output_file = "data/non_blank_categories.csv"
 #utils.export_dataframe_to_csv(filtered_df, output_file)
 
+'''
+STEP 2 - check columns with data that are not meaningful
+'''
 #drop occupationId column as it contains no data
 filtered_df = filtered_df.drop(columns=['occupationId'])
 
@@ -55,6 +63,9 @@ utils.check_dataframe_for_blanks(filtered_df)
 output_file = "data/filtered_SGJobData.csv"
 #utils.export_dataframe_to_csv(filtered_df, output_file)
 
+'''
+STEP 3 - check data accuracy for min/max/avg salary
+'''
 #check data accuracy for min/max salary
 salary_df = filtered_df[['salary_minimum','salary_maximum','average_salary']].astype(np.float64)
 invalid_salary_mask = (salary_df['salary_minimum'] > salary_df['salary_maximum'])
@@ -66,15 +77,43 @@ invalid_avg_salary_mask = (salary_df['average_salary'] < salary_df['salary_minim
 invalid_avg_salary_df = salary_df[invalid_avg_salary_mask]
 print("\nNumber of entries with invalid average salary data:", invalid_avg_salary_df.size, "\nshape:", invalid_avg_salary_df.shape)
 
+'''
+STEP 4 - check for any duplicate job post id with "metadata_jobPostId" column
+'''
 #check for any duplicate job post id
 print("duplicates rows found:",filtered_df.duplicated().sum())
 print("duplicates job post ids found:",filtered_df.duplicated(subset=['metadata_jobPostId']).sum())
 
+'''
+STEP 5 - check if text columns (postedCompany_name, title) contain inconsistent cases and special characters
+'''
 #check if company name contains similar entries with different cases or symbols
 company_names = filtered_df['postedCompany_name'].unique()
 company_name_df = pd.DataFrame(company_names, columns=['postedCompany_name'])
 company_name_df.sort_values(by=['postedCompany_name'], inplace=True)
 #utils.export_dataframe_to_csv(company_name_df,"data/unique_company_names.csv")
+company_name_df['postedCompany_name'] = company_name_df['postedCompany_name'].str.upper()
+#utils.export_dataframe_to_csv(company_name_df,"data/unique_company_names_upper.csv")
+
+#replace ampersand from company name and title
+filtered_df['title'] = filtered_df['title'].str.replace("&", "and")
+filtered_df['postedCompany_name'] = filtered_df['postedCompany_name'].str.replace("&", "and")
+
+# Define regex pattern for special characters
+# [^a-zA-Z0-9\s] means "any character NOT a letter, digit, whitespace, or period"
+special_chars_pattern = r'[^a-zA-Z0-9\s.]'
+
+# Clean the 'Title' and 'Company Name' columns
+filtered_df['title'] = filtered_df['title'].str.replace(special_chars_pattern, '', regex=True)
+filtered_df['postedCompany_name'] = filtered_df['postedCompany_name'].str.replace(special_chars_pattern, '', regex=True)
+
+# Remove leading and trailing whitespace
+filtered_df['title'] = filtered_df['title'].str.strip()
+filtered_df['postedCompany_name'] = filtered_df['postedCompany_name'].str.strip()
+
+# Replace multiple spaces with a single space
+filtered_df['title'] = filtered_df['title'].str.replace(r'\s+', ' ', regex=True)
+filtered_df['postedCompany_name'] = filtered_df['postedCompany_name'].str.replace(r'\s+', ' ', regex=True)
 
 # For filtered_df dataframe, convert postedCompany_name column values to uppercase (as strings)
 filtered_df['postedCompany_name'] = filtered_df['postedCompany_name'].str.upper()
@@ -82,11 +121,59 @@ filtered_df['postedCompany_name'] = filtered_df['postedCompany_name'].str.upper(
 output_file = "data/filtered_SGJobData.csv"
 #utils.export_dataframe_to_csv(filtered_df, output_file)
 
+'''
+STEP 6 - identify the employment types of jobs to focus on
+'''
+# group jobs by employment type
+employment_type_groupings = filtered_df.groupby('employmentTypes').size().reset_index(name='Count')
+employment_type_groupings['Percentage of jobs'] = employment_type_groupings['Count'].astype(np.float64) / len(filtered_df) * 100
+employment_type_groupings.sort_values(by='Count', ascending=False, inplace=True)
+print("Using size():\n", employment_type_groupings)
+
+'''
+based on employment types, to focus only on 95% of jobs:
+- Permanent (44%), Full Time (38%) and Contract (13%)
+'''
+employment_type_mask = (filtered_df['employmentTypes'].str.fullmatch('Permanent') 
+                        | filtered_df['employmentTypes'].str.fullmatch('Full Time') 
+                        | filtered_df['employmentTypes'].str.fullmatch('Contract'))
+filtered_df = filtered_df[employment_type_mask]
+
+'''
+to remove jobs with titles that are meant for excluded employment types e.g. parttime/part time, intern, temp
+to remove jobs with titles that contain hourly rates
+'''
+# regex pattern to identify hourly paid jobs
+hourly_paid_jobs_pattern = r'\d+(\.\d+)?\s*(hour|hr|per hour|per hr)'
+
+exclude_job_title_mask = (filtered_df['title'].str.contains('parttime', case=False, na=False) 
+                           | filtered_df['title'].str.contains('part time', case=False, na=False) 
+                           | filtered_df['title'].str.contains('intern', case=False, na=False) 
+                           | filtered_df['title'].str.contains('temp', case=False, na=False)
+                           | filtered_df['title'].str.contains('freelance', case=False, na=False)
+                           | filtered_df['title'].str.contains('flexible', case=False, na=False)
+                           | filtered_df['title'].str.contains(hourly_paid_jobs_pattern, case=False, na=False, regex=True)
+                        )
+
+filtered_df = filtered_df[~exclude_job_title_mask]
+
+'''
+to remove outliers where average salary > 400,000
+to remove low paying jobs where average salary < 4,000
+'''
+#exclude average salary upper bound for outliers, and exclude jobs where average salary is below 4000
+exclude_avg_salary_mask = (filtered_df['average_salary'] > 400000) | (filtered_df['average_salary'] < 4000)
+filtered_df = filtered_df[~exclude_avg_salary_mask]
+
+#export filtered dataframe to csv
+output_file = "data/filtered_SGJobData_above_4k_salary.csv"
+utils.export_dataframe_to_csv(filtered_df, output_file)
+
+
 #check outliers for salary columns (min, max, average)
+salary_df = filtered_df[['salary_minimum','salary_maximum','average_salary']].astype(np.float64)
 print("\nDescribe salary dataframe:\n", salary_df.describe(),"\n", salary_df.info())
 print(salary_df.head())
-
-#map employment type to main categories
 
 
 #map job level to main categories
